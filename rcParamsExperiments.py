333aaa796e9da1fc70e8a0fa37da22778f3d4dc7
Matthew Fernandez <33529858+mattf4171@users.noreply.github.com>
2021-04-08 13:57:36 -0700

    Update rcParamsExperiments.py

--- rcParamsExperiments.py
+++ rcParamsExperiments.py
@@ -30,3 +30,44 @@ plt.rc('lines', linewidth=2)
 
 for i in range(4):
     plt.plot(np.random.rand(10))
+    
+    
+plt.style.available[:5] # view the available top 5 stylesheets
+
+def hist_and_lines():
+    # fn to create two basic types of plots
+    np.random.seed(0)
+    fig, ax= plt.subplots(1,2, figsize=(11, 4))
+    ax[0].hist(np.random.randn(1000))
+    for i in range(3):
+        ax[1].plot(np.random.rand(10))
+    ax[1].legend(['a','b', 'c'], loc='lower left')
+
+with plt.style.context('fivethirtyeight'): 
+    # fivethirtyeight will incorporate bold colors, thick lines and transparent axes
+    hist_and_lines()
+
+
+
+with plt.style.context('ggplot'): 
+    # ggplot resembles R lang vizualization tool, mimics default style from that pkg
+    hist_and_lines()
+    
+    
+with plt.style.context('bmh'):
+    # bmh stylesheet mimics the 'Probabilistic Programming and Bayesian Methods 
+    # for Hackers' with rcParams to create consistent and visually appealing styles
+    hist_and_lines()
+    
+    
+with plt.style.context('dark_background'):
+    # Great for presentations, useful with dark backgrounds
+    hist_and_lines()
+    
+with plt.style.context('grayscale'):
+    # great when printed w/o color
+    hist_and_lines()
+    
+    
+    
+    
